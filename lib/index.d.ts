91c11dda2029d7635700efcb439b5e59cbff7743
Daniel Almeida <daniel.almeida11@kraftheinz.com>
2022-10-09 20:07:13 -0300

    Updated lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
-export declare const importFileandPreview: (file: File, revoke?: boolean | undefined) => Promise<string>;
-export declare const generateVideoThumbnails: (videoFile: File, numberOfThumbnails: number, type: string) => Promise<string[]>;
+export declare const importFileandPreview: (file: File, revoke?: boolean) => Promise<string>;
+export declare const generateVideoThumbnails: (videoFile: File, numberOfThumbnails: number, type: string, cb?: ((thumbnail: string, index: number) => void) | undefined) => Promise<string[]>;
 export declare const getVideoCover: (urlOfFIle: string, seekTo?: number) => Promise<string>;
 export declare const generateVideoThumbnailViaUrl: (urlOfFIle: string, videoTimeInSeconds: number) => Promise<string>;
 export declare const getVideoDurationFromVideoFile: (videoFile: File | string) => Promise<number>;
